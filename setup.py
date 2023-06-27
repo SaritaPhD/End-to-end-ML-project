a3426f3dc8673b042f749cf2d620613c60e25ea2
SaritaPhD <sarita@ma.iitr.ac.in>
2023-06-27 18:46:10 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hyphen_E_dot = '-e .'
+def get_requirements(file_path: str)->List[str]:
+    '''This function will return the list of requirements'''
+    requiremets = []
+    with open(file_path) as file_obj:
+        requiremets = file_obj.readlines()
+        requiremets = [req.replace('/n', '') for req in requiremets]
+
+        if hyphen_E_dot in requiremets:
+            requiremets.remove(hyphen_E_dot)
+
+    return requiremets
+
+setup(
+    name='MLproject',
+    version='0.0.1',
+    author='Sarita',
+    author_email='sarita@ma.iitr.ac.in',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
